aeeb91a2612f01ca0ce002f66fa5c15e2a04bee9
Charles Ngalasa <64107249+GuruZM@users.noreply.github.com>
2024-01-15 11:04:27 +0200

    changed the url

--- resources/js/Axios/axiosConfig.jsx
+++ resources/js/Axios/axiosConfig.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://admin.premier-petroleum.com/api/v1', 
+  baseURL: 'https://inventory.cozyhouseinterior.com/api/v1', 
   //  baseURL: 'http://localhost:8000/api/v1', 
   timeout: 5000, 
 });
