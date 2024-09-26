631544a977f9d21d1b9512a08d10ac64dcc805b6
Charles Ngalasa <64107249+GuruZM@users.noreply.github.com>
2024-09-27 00:23:03 +0200

    all changes done as requested 2

--- resources/js/Axios/axiosConfig.jsx
+++ resources/js/Axios/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    // baseURL: "https://admin.premier-petroleum.com/api/v1",
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL: "https://admin.premier-petroleum.com/api/v1",
+    // baseURL: "http://localhost:8000/api/v1",
     timeout: 5000,
 });
 
